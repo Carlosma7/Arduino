69c81fbfab57851aac7a816e3d1493d0d0ed3b04
Carlos Morales Aguilera <carlos7ma@gmail.com>
2023-01-30 23:56:19 +0100

    Fix comments

--- Components/17. Potentiometer/Potentiometer.ino
+++ Components/17. Potentiometer/Potentiometer.ino
@@ -1,14 +1,15 @@
-// the setup routine runs once when you press reset:
-void setup() {
-  // initialize serial communication at 9600 bits per second:
+/*
+  Potentiometer
+
+  It gets the analogic value from the potentiometer and prints it on the serial monitor.
+*/
+
+void setup(){
   Serial.begin(9600);
 }
 
-// the loop routine runs over and over again forever:
-void loop() {
-  // read the input on analog pin 0:
+void loop(){
   int sensorValue = analogRead(A0);
-  // print out the value you read:
   Serial.println(sensorValue);
-  delay(200);  // delay in between reads for stability
+  delay(200);
 }
